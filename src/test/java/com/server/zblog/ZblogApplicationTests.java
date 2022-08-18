714bc12256b048f3b9e8d0a6db192d319579bc72
zexoverz <faisalfirdani01@gmail.com>
2022-08-18 01:03:05 -0500

    Server Initiallze

---
+++ src/test/java/com/server/zblog/ZblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.zblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
